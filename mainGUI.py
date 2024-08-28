8e6bc6088af30d8214dc247ffc595a7b23f31644
Nelson Malbone <nhmalbone@icloud.com>
2024-08-28 18:49:48 -0400

    08/28/2024 Brocode project # 5 using pysimplegui to make a interest compound calc

---
+++ mainGUI.py
@@ -0,0 +1,15 @@
+# make a gui interest calc
+import PySimpleGUI as sg
+
+def main():
+# title of project
+    window = sg.Window(title="Compound Interest Calculator",
+              layout=[[]])
+    while True:
+        event, values = window.read()
+        # end program if user closes window
+        if event == "OK" or event == sg.WIN_CLOSED:
+            break
+
+if __name__ == "__main__":
+    main()

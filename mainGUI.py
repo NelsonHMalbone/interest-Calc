fbcda6b90f3a6c4b44f999e48c200702a5741335
Nelson Malbone <nhmalbone@icloud.com>
2024-08-28 18:50:19 -0400

    08/28/2024 Brocode project # 5 adjusting the spaces

--- mainGUI.py
+++ mainGUI.py
@@ -1,15 +1,17 @@
 # make a gui interest calc
 import PySimpleGUI as sg
 
+
 def main():
-# title of project
+    # title of project
     window = sg.Window(title="Compound Interest Calculator",
-              layout=[[]])
+                       layout=[[]])
     while True:
         event, values = window.read()
         # end program if user closes window
         if event == "OK" or event == sg.WIN_CLOSED:
             break
+
 
 if __name__ == "__main__":
     main()

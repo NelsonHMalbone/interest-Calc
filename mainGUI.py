d9ada05c19e70a42d3279a2686ae0b034c9165f2
Nelson Malbone <nhmalbone@icloud.com>
2024-09-02 18:02:01 -0400

    08/28/2024 Brocode project # 5 set up the layout with the layout variables.

--- mainGUI.py
+++ mainGUI.py
@@ -1,11 +1,27 @@
 # make a gui interest calc
 import PySimpleGUI as sg
 
+# using 3 vars so the user can fill in to get
+#the total of time and rate of interest
+
+
+principleInputText = sg.Text("Enter principle amount: ")
+principleInput = sg.InputText()
+
+rateInputText = sg.Text("Enter interest rate: ")
+rateInput = sg.InputText()
+
+timeInputText = sg.Text("Enter time in years: ")
+timeInput = sg.InputText()
+
+layout=[[principleInputText,principleInput],
+        [rateInputText, rateInput],
+        [timeInputText,timeInput]]
 
 def main():
     # title of project
     window = sg.Window(title="Compound Interest Calculator",
-                       layout=[[]])
+                       layout= layout)
     while True:
         event, values = window.read()
         # end program if user closes window

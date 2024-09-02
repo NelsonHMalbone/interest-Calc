90c6b7105eefac8c6a2a64a6fdf330c9a66f66b3
Nelson Malbone <nhmalbone@icloud.com>
2024-09-02 18:21:56 -0400

    09/02/2024 Brocode project # 5 set up while loops

--- mainGUI.py
+++ mainGUI.py
@@ -4,6 +4,9 @@ import PySimpleGUI as sg
 # using 3 vars so the user can fill in to get
 #the total of time and rate of interest
 
+principle = 0
+rate = 0
+time = 0
 
 principleInputText = sg.Text("Enter principle amount: ")
 principleInput = sg.InputText()
@@ -13,6 +16,8 @@ rateInput = sg.InputText()
 
 timeInputText = sg.Text("Enter time in years: ")
 timeInput = sg.InputText()
+
+complete_button = sg.Button("Complete")
 
 layout=[[principleInputText,principleInput],
         [rateInputText, rateInput],
@@ -24,6 +29,22 @@ def main():
                        layout= layout)
     while True:
         event, values = window.read()
+        while principle <= 0:
+            principleInput
+            if principle <= 0:
+                print("principle cant be less or equal to 0")
+
+        while rate <= 0:
+            rateInput
+            if rate <= 0:
+                print("Interest rate cant be less or equal to 0")
+
+        while time <= 0:
+            timeInput
+            if time <= 0:
+                print("Time cant be less or equal to 0")
+
+
         # end program if user closes window
         if event == "OK" or event == sg.WIN_CLOSED:
             break

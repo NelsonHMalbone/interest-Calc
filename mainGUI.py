dcc83a4c3cf39683d7548edea4a36bf63febf856
Nelson Malbone <nhmalbone@icloud.com>
2024-09-03 03:52:34 -0400

    09/02/2024 Brocode project # 5 got rid of the def functions now i just get a 0 of years and 0.00 for the total

--- mainGUI.py
+++ mainGUI.py
@@ -1,28 +1,11 @@
 # make a gui interest calc
 import PySimpleGUI as sg
 
+
 def main():
     principle = 0
     rate = 0
     time = 0
-
-    def getPrinicple(principleInput):
-        while principle <= 0:
-            principle = float(principleInput)
-            if principle <= 0:
-                print("principle cant be less or equal to 0")
-
-    def getRate(rateInput):
-        while rate <= 0:
-            rate = float(rateInput)
-            if rate <= 0:
-                print("Interest rate cant be less or equal to 0")
-
-    def getTime(timeInput):
-        while time <= 0:
-            time = int(timeInput)
-            if time <= 0:
-                print("Time cant be less or equal to 0")
 
     # using 3 vars so the user can fill in to get
     # the total of time and rate of interest
@@ -37,7 +20,7 @@ def main():
     timeInput = sg.InputText(key='times')
 
     complete_button = sg.Button("Complete")
-    output_label = sg.Text(key='output', text_color="green")
+    output_label = sg.Text(key='output', text_color="black")
 
     layout = [[principleInputText, principleInput],
               [rateInputText, rateInput],
@@ -53,14 +36,25 @@ def main():
         print(event, values)
         match event:
             case 'principles':
-                principle = getPrinicple(principleInput)
+                while principle <= 0:
+                    principle = float(input(principleInput))
+                    if principle <= 0:
+                        print("principle cant be less or equal to 0")
+
             case 'rates':
-                rate = getRate(rateInput)
+                while rate <= 0:
+                    rate = float(input(rateInput))
+                    if rate <= 0:
+                        print("Interest rate cant be less or equal to 0")
             case 'times':
-                time = getTime(timeInput)
+                while time <= 0:
+                    time = int(input(timeInput))
+                    if time <= 0:
+                        print("Time cant be less or equal to 0")
+
             case 'Complete':
                 total = principle * pow((1 + rate / 100), time)
-
+                print(total)
                 window['output'].update(value=f'Balance after {time} year/s: ${total: .2f}')
         # end program if user closes window
         if event == "OK" or event == sg.WIN_CLOSED:
